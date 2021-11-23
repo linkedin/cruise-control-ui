f7732ca8d812406f3b6f1a13b821b2f48f5fe721
christofluethi <christofluethi@users.noreply.github.com>
2021-11-23 15:51:03 +0100

    allow deployment of CCFE to a context root different to '/' by adding the cc-logo and favicon images as assets and setting the assetPublicPath to ''.

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -30,6 +30,7 @@ module.exports = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon: path.resolve(__dirname, '../src/assets/images/cc-favicon.png'),
       inject: true
     }),
     new FriendlyErrorsPlugin()
--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -28,7 +28,7 @@ module.exports = {
     port: 8090,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     proxyTable: {
       // route the requests to cruise-control backend
       '/kafkacruisecontrol': {
